8c4c642b0b2d4042159d9d546d0a196ce72444a3
suresanand89 <suresanand@gmail.com>
2016-08-14 15:52:48 -0700

    more login

--- app/src/main/java/com/rooztr/rooztr_android/SplashActivity.java
+++ app/src/main/java/com/rooztr/rooztr_android/SplashActivity.java
@@ -1,17 +1,12 @@
 package com.rooztr.rooztr_android;
 
-import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.os.Handler;
-import android.support.annotation.Nullable;
-import android.support.v7.app.ActionBarActivity;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
 import com.rooztr.helper.RequestHandler;
@@ -25,10 +20,9 @@ public class SplashActivity extends Activity {
 
 
     private static int SPLASH_TIME_OUT = 3000;
-    private Button send;
+    private Button send,login;
     private EditText phoneNumber,code;
-    private LinearLayout linearLayout;
-    private static String phoneNo = null;
+    private static String phoneNo = null,messageCode,token;
     private RelativeLayout relativeLayout;
 
     @Override
@@ -37,25 +31,61 @@ public class SplashActivity extends Activity {
 
         setContentView(R.layout.splash_layout);
 
+        SharedPreferences prefs = getApplicationContext().getSharedPreferences("tokenstorage", 0);
+        token = prefs.getString("token","");
+        phoneNo = prefs.getString("Phone Number","");
+        if(token.length() != 0 && phoneNo.length() != 0) {
+            try {
+                if(RequestHandler.validate(phoneNo, token)) {
+                   Intent intent = new Intent(SplashActivity.this,MainActivity.class);
+                   startActivity(intent);
+                }
+            }catch(Exception e) {
+
+            }
+        }
+
+
         send = (Button) findViewById(R.id.b1);
         phoneNumber = (EditText) findViewById(R.id.p2);
-        linearLayout = (LinearLayout) findViewById(R.id.l1);
         code = (EditText) findViewById(R.id.p3);
         relativeLayout = (RelativeLayout) findViewById(R.id.relative);
-
-
-        phoneNo = phoneNumber.getText().toString();
+        login = (Button) findViewById(R.id.login);
 
         send.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                phoneNo = phoneNumber.getText().toString();
                 send.setText("RESEND");
                 relativeLayout.setVisibility(View.VISIBLE);
-                RequestHandler.register(phoneNo);
+                try {
+
+                    RequestHandler.register(phoneNo);
+
+                }catch (Exception e) {
+
+                }
             }
         });
 
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+                messageCode = code.getText().toString();
+                try {
+                    token = RequestHandler.login(phoneNo,messageCode);
+                    SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("tokenstorage",0);
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putString("Phone Number",phoneNo);
+                    editor.putString("token",token);
+                    editor.apply();
+
+                }catch (Exception e) {
+
+                }
+            }
+        });
 
     }
 }
